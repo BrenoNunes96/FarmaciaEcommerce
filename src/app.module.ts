64b127b02fcb75c5face04f4a909028b81ec10d3
BrenoNunes96 <brenocp3@live.com>
2026-03-17 11:49:57 -0300

    modulo adicionado na appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { categoria } from './categoria/entities/categoria.entity';
+import { categoriaModule } from './categoria/categoria.module';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type:'mysql',
@@ -14,8 +15,8 @@ import { categoria } from './categoria/entities/categoria.entity';
     entities:[categoria],
     database:'db_farmacia'
 
-  })
-
+  }),
+categoriaModule
 ],
   controllers: [AppController],
   providers: [AppService],
